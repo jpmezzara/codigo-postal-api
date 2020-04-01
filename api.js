5c1046a8c6d1cea6f0afa13cd6d796ea0b20ab6d
JP <jpmezzara@me.com>
2020-03-31 18:03:35 -0600

    Refacotr API, new urls

--- api.js
+++ api.js
@@ -1,46 +1,19 @@
-'use-strict'
+const Api = require('claudia-api-builder');
+const estados = require('./data/estados');
+const cps = require('./data/cps');
 
-const Api = require('claudia-api-builder')
-
-const api = new Api()
-
-const estados = [
-    { id: 1, estado: 'Aguascalientes' },
-    { id: 2, estado: 'Baja California' },
-    { id: 3, estado: 'Baja California Sur' },
-    { id: 4, estado: 'Campeche' },
-    { id: 5, estado: 'Coahuila' },
-    { id: 6, estado: 'Colima' },
-    { id: 7, estado: 'Chiapas' },
-    { id: 8, estado: 'Chihuahua' },
-    { id: 9, estado: 'CDMX' },
-    { id: 10, estado: 'Durango' },
-    { id: 11, estado: 'Guanajuato' },
-    { id: 12, estado: 'Guerrero' },
-    { id: 13, estado: 'Hidalgo' },
-    { id: 14, estado: 'Jalisco' },
-    { id: 15, estado: 'México' },
-    { id: 16, estado: 'Michoacán' },
-    { id: 17, estado: 'Morelos' },
-    { id: 18, estado: 'Nayarit' },
-    { id: 19, estado: 'Nuevo León' },
-    { id: 20, estado: 'Oaxaca' },
-    { id: 21, estado: 'Puebla' },
-    { id: 22, estado: 'Querétaro' },
-    { id: 23, estado: 'Quintana Roo' },
-    { id: 24, estado: 'San Luis Potosí' },
-    { id: 25, estado: 'Sinaloa' },
-    { id: 26, estado: 'Sonora' },
-    { id: 27, estado: 'Tabasco' },
-    { id: 28, estado: 'Tamaulipas' },
-    { id: 29, estado: 'Tlaxcala' },
-    { id: 30, estado: 'Veracruz' },
-    { id: 31, estado: 'Yucatán' },
-    { id: 32, estado: 'Zacatecas' }
-]
+const api = new Api();
 
 api.get('/estados', () => {
     return estados
-})
+});
 
-module.exports = api
+api.get('/estados/{id}/municipios', (request) => {
+    return cps.filter(codigo => codigo.idEstado === request.pathParams.id);
+});
+
+api.get('/cp/{cp}', (request) => {
+    return cps.filter(codigo => codigo.cp === request.pathParams.cp);
+});
+
+module.exports = api;
